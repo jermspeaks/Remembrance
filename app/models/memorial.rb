e814006c4d8b51e66598d5ec07415fc28d22e001
JYoung217 <JonYoungg@gmail.com>
2014-08-14 13:15:01 -0500

    finished 3 models: memorial, comment, memorial_guest

--- app/models/memorial.rb
+++ app/models/memorial.rb
@@ -1,2 +1,7 @@
 class Memorial < ActiveRecord::Base
+	belongs_to :moderator, class_name: User
+	has_many :posts
+	has_many :photos
+	has_many :comments, as: :commentable
+	has_many :guests, through: :memorial_guests
 end
