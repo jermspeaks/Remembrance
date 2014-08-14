bed9c5c4aab7f8cb044268867c6a59da2df2ea85
gond0026@umn.edu <gond0026@umn.edu>
2014-08-14 15:02:17 -0500

    jolly

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 
   belongs_to :author, class_name: "User"
   belongs_to :memorial
-  has_many :comments, as :commentable
+  has_many :comments, as: :commentable
 
 end
