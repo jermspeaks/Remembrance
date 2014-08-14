7600ad6c05bae2643099c5a72d72f7bb35a72025
JYoung217 <JonYoungg@gmail.com>
2014-08-14 18:43:34 -0500

    ignore

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
-class Post < ActiveRecord::Base3
-=======
 class Post < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   belongs_to :memorial
   has_many :comments, as: :commentable
 
   validates :text, presence: true
->>>>>>> 12a605440b270ac6737d398591e30fb9cbae86be
+
 end
