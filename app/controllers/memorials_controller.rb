5d89d4a180c567637607fe40e98f8c4e855e2bdc
gond0026@umn.edu <gond0026@umn.edu>
2014-08-15 19:43:15 -0500

    added some more views

--- app/controllers/memorials_controller.rb
+++ app/controllers/memorials_controller.rb
@@ -1,15 +1,29 @@
 class MemorialsController < ApplicationController
 	include ApplicationHelper
 
-	def new 
-		@memorial = Memorial.new
-	end
-
 	def index
 		current_user
 		@attended_memorials = @current_user.attended_memorials
-		@created_memorials = @current_user.created_memorials 
+		@created_memorials = @current_user.created_memorials
 	end
+
+	def new
+		@memorial = Memorial.new
+  end
+
+  def create
+    @memorial = Memorial.new(memorial_params)
+    if @memorial.save
+      redirect_to @memorial
+    else
+      render 'newmemorial'
+    end
+  end
+
+	def show
+		@memorial = Memorial.find(params[:id])
+	end
+
 
 	def created
 
@@ -19,8 +33,10 @@ class MemorialsController < ApplicationController
 
 	end
 
-	def show
-		@memorial = Memorial.find params[:id]
-	end
+	private
+    def memorial_params
+      params.require(:memorial).permit(:deceased_name, :date_of_birth, :date_of_death, :obituary_description, :service_description)
+    end
+
 
 end
