21d5200ea51636b5e801ef01581bf1f32e8aacf2
jermspeaks <jermspeaks@gmail.com>
2014-08-16 15:22:02 -0500

    spec tests are green for login

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,34 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, :type => :controller do
-	describe '#new' do
+	describe '#index' do
 		it 'should render the homepage' do
-			get :new
+			get :index
 			expect(response).to be_success
 			expect(response.code).to eq("200")
 		end
 
-		it 'should redirect to memorials path if user is session exists' do
+		it 'should redirect to memorials path if a user session exists' do
 			@user = User.create(name: "Frank", email_address: "frank@example.com", password: "123456", password_confirmation: "123456")
 			session[:user_id] = @user.id
-			get :new
+			get :index
 			expect(response).to be_redirect
 			expect(response.code).to eq("302")
 		end
 	end
 
 	describe '#create' do
-		before do
-		  request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:facebook]
+		it 'should allow a user to log in' do
+			@user = User.create(name: "Frank", email_address: "frank@example.com", password: "123456", password_confirmation: "123456")
+			post :create, user: {email_address: @user.email_address, password: @user.password}
+			expect(response).to be_redirect
+			expect(response.code).to eq("302")
+			expect(session[:user_id]).to eq(@user.id)
 		end
 
-		it 'should create a new user' do
-			expect(request.env['omniauth.auth']).to be_kind_of(Hash)
-			request.env['omniauth.auth']['credentials']['expires_at'] = Time.now + 1.week
-	    post :create
-			expect(User.last.email_address).to eq("jeremy_rhvuhfx_wongwong@tfbnw.net")
+		it 'should not allow a user to log in with false credentials' do
+			post :create, user: {email_address: "notauser@email.com", password: "12345"}
+			expect(response).to be_success
+			expect(response.code).to eq("200")
+			expect(session[:user_id]).to be_nil
 		end
-	end
+
+	end	
 
 	describe '#destroy' do
 		it 'should log out a user' do
