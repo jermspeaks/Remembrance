0bee28188c0cf9513b7ac5900d6328b2f3afef57
jermspeaks <jermspeaks@gmail.com>
2014-08-15 20:20:57 -0500

    added sessions controller specs, green

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -7,12 +7,13 @@ RSpec.describe SessionsController, :type => :controller do
 			expect(response).to be_success
 			expect(response.code).to eq("200")
 		end
+
 		it 'should redirect to memorials path if user is session exists' do
 			@user = User.create(name: "Frank", email_address: "frank@example.com")
 			session[:user_id] = @user.id
 			get :new
 			expect(response).to be_redirect
-			# expect(response.code).to eq("302")
+			expect(response.code).to eq("302")
 		end
 	end
 
@@ -20,9 +21,23 @@ RSpec.describe SessionsController, :type => :controller do
 		before do
 		  request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:facebook]
 		end
+
 		it 'should create a new user' do
-	 #    post :create, auth: env
-		# 	expect(User.last.email_address).to eq("jeremy_rhvuhfx_wongwong@tfbnw.net")
+			expect(request.env['omniauth.auth']).to be_kind_of(Hash)
+			request.env['omniauth.auth']['credentials']['expires_at'] = Time.now + 1.week
+	    post :create
+			expect(User.last.email_address).to eq("jeremy_rhvuhfx_wongwong@tfbnw.net")
 		end
 	end
+
+	describe '#destroy' do
+		it 'should log out a user' do
+			user = User.create(name: "Frank", email_address: "frank@example.com")
+			session[:user_id] = user.id
+			expect(session[:user_id]).to_not be_nil
+	    delete :destroy
+			expect(session[:user_id]).to be_nil
+		end
+	end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,10 +10,13 @@ omniauth_hash =
     { provider: 'facebook',
 	    uid: '100006598157037',
 			info: {
-				email: 'jeremy_rhvuhfx_wongwong@tfbnw.net'
+				email: 'jeremy_rhvuhfx_wongwong@tfbnw.net',
+				name: 'Jeremy Wongwong'
 				},
 			credentials: {
-        token: 'CAAGrm0zGx0UBADjUS1pHLOvIZCyerrkFrRr8E04sIs8gwEH8ZBvZCEhZCePCc7ucZCuppfZAHWBBOYsd2FwRx1T0Qf5E4rAHZBMV4WWB4BDeOwJbpen4nZAVnMjXDUcO67jZBPeDr70rMzDFGVEinA19oaSuoGtywZCBVVLwz5F0HSVeyBD147JVD6xITzpUozZAOqzbJXeeTX75lfMqNifZCi3SScwe0uyMX7gZD'
+        token: 'CAAGrm0zGx0UBADjUS1pHLOvIZCyerrkFrRr8E04sIs8gwEH8ZBvZCEhZCePCc7ucZCuppfZAHWBBOYsd2FwRx1T0Qf5E4rAHZBMV4WWB4BDeOwJbpen4nZAVnMjXDUcO67jZBPeDr70rMzDFGVEinA19oaSuoGtywZCBVVLwz5F0HSVeyBD147JVD6xITzpUozZAOqzbJXeeTX75lfMqNifZCi3SScwe0uyMX7gZD',
+        expires_at: Time.now + 1.week,
+        expires: true
       }
 	  }
 OmniAuth.config.add_mock(:facebook, omniauth_hash)
