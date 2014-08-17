e505197888bca3c3a8be2afcb1eaf0ceea307566
jermspeaks <jermspeaks@gmail.com>
2014-08-17 15:15:05 -0500

    heart pain, and comment tests done

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -17,11 +17,47 @@ RSpec.describe CommentsController, :type => :controller do
 
   describe 'POST #create' do
     it 'should save a new comment' do
-      post :create, :memorial_id => @memorial.id, :post_id => @post.id, :comment => { author: @user, post: @post, text: "Sample comment" }
+      post :create, :memorial_id => @memorial.id, :post_id => @post.id, :comment => { commenter_id: @user.id, post: @post, text: "Sample comment" }
       expect(response.code).to eq("302")
       expect(Comment.last.text).to eq("Sample comment")
     end
   end
+  describe 'Modifying comment' do
+    before do
+      @user = User.create(name: "Frank", email_address: "frank@example.com", password: "123456", password_confirmation: "123456")
+      @memorial = Memorial.create(moderator: @user, deceased_name: "Peters")
+      @post = Post.create(author: @user, memorial: @memorial, approved: true, text: "Sorry about loss")
+      @comment = Comment.create!(commenter: @user, commentable_id: @post.id, commentable_type: 'Post', text: "Sample comment")
+    end
 
+    context 'GET #edit' do
+      it 'should render a form to edit the comment' do
+        get :edit, :memorial_id => @memorial.id, :post_id => @post.id, :id => @comment.id
+        expect(response).to be_success
+        expect(response.code).to eq("200")
+      end
+    end
+
+    context 'PUT #update' do
+      it 'should edit the comment' do
+        post :update, :memorial_id => @memorial.id, :post_id => @post.id, :id => @comment.id, :comment => { text: "Sorry" }
+        expect(response.code).to eq("302")
+        expect(Comment.last.text).to eq("Sorry")
+      end
+    end
+
+    context 'DELETE #destroy' do
+      it "destroys the requested comment" do
+        expect {
+          delete :destroy, {memorial_id: @memorial.id, :post_id => @post.id, :id => @comment.id}
+        }.to change(Comment, :count).by(-1)
+      end
+
+      it "redirects to the memorial page" do
+        delete :destroy, {memorial_id: @memorial.id, :post => @post.id, :id => @comment.id}
+        expect(response.code).to eq("302")
+      end
+    end
+  end
 
 end
