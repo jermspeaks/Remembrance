91b69dce7f697f9241b0fffd3d87ed2d105d2954
gond0026@umn.edu <gond0026@umn.edu>
2014-08-17 14:24:48 -0500

    stashing comments controller plus views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,27 @@
 class CommentsController < ApplicationController
+  include ApplicationHelper
+
+  def index
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    current_user
+    @post = Post.find(params[:post_id])
+    @memorial = Memorial.find(params[:memorial_id])
+    @comment = @post.comments.create(comment_params)
+    @comment.update(commenter: @current_user)
+    redirect_to memorial_path(@memorial)
+  end
+
+  def show
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :commentable, :text)
+    end
 end
