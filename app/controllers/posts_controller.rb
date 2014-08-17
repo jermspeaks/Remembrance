48e71db542bb89a6b3818a624d1a891db8252ad8
gond0026@umn.edu <gond0026@umn.edu>
2014-08-17 13:14:57 -0500

    test test test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,12 +19,23 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+      if @post.update(post_params)
+        redirect_to memorials_path
+      else
+        render 'edit'
+      end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to memorials_path
   end
 
   private
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -22,10 +22,33 @@ RSpec.describe PostsController, :type => :controller do
 		end
 	end
 
-	describe 'GET #edit'
+	describe 'GET #edit' do
+		it 'should render a form to edit the post' do
+			post :edit, :memorial_id => @memorial.id, 
+			expect(response).to be_success
+			expect(response.code).to eq("200")
+		end
+	end
 
-	describe 'PUT #update'
+	describe 'PUT #update' do
+		it 'should edit the post' do
+			post :update, :memorial_id => @memorial.id, :post => { author: @user, memorial: @memorial, approved: true, text: "Sorry about loss" }
+			expect(Post.last.text).to eq("Sorry about loss")
+	end
 
-	describe 'DELETE #destroy'
+	describe 'DELETE #destroy' do
+		it "destroys the requested post" do
+       post = Post.create! valid_attributes
+        expect {
+          delete :destroy, {:id => post.to_param}, valid_session
+        }.to change(post, :count).by(-1)
+    end
+
+    it "redirects to the memorial page" do
+      post = Post.create! valid_attributes
+      delete :destroy, {:id => post_params}, valid_session
+      response.should redirect_to(memorials_path(@memorial))
+    end
+	end
 
 end
