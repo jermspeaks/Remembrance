7380828ceea6a515119ea30c256e26b54709481e
jermspeaks <jermspeaks@gmail.com>
2014-08-18 17:51:18 -0500

    creating the correct query call

--- app/controllers/memorials_controller.rb
+++ app/controllers/memorials_controller.rb
@@ -3,13 +3,13 @@ include ApplicationHelper
 class MemorialsController < ApplicationController
   def index
     current_user
-    @attended_memorials = []
-    MemorialGuest.all.each do |record|
-      if @current_user.id == record.guest_id
-        attended << Memorial.find(record.memorial_id)
-      end
-    end
-
+    attend = @current_user.attended_memorials
+    created = @current_user.created_memorials
+    @attended_memorials = attend + created
+    @post = Post.where("memorial_id IN (?)", @attended_memorials.map(&:id)).order("created_at DESC").limit(5)
+    binding.pry
+    # find all posts in memorials, order by recent, limit 5
+    # find all photos in memorials, order by recent, limit 5
   end
 
   def show
