9908628fc13fa68402dcb0abd5dbe4842a05cb2b
jermspeaks <jermspeaks@gmail.com>
2014-08-19 14:55:47 -0500

    state machines

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,29 +6,27 @@ class Post < ActiveRecord::Base
 
   validates :text, presence: true
 
-
   state_machine initial: :published do
     state :published, value: 0
     state :flagged, value: 1
     state :deleted, value: 2
-  end
 
-  event :flag do
-    transition :published => :flagged
-  end
+    event :flag do
+      transition :published => :flagged
+    end
 
-  event :delete do
-    transition :published => :deleted
-  end
+    event :delete do
+      transition :published => :deleted
+    end
 
-  event :approve do
-    transition :flagged => :published
-  end
+    event :approve do
+      transition :flagged => :published
+    end
 
-  event :accident do
-    transition :deleted => :published
+    event :accident do
+      transition :deleted => :published
+    end
   end
-end
 
 
 
