e787850b9c3848a17758118f0fa5b7782ea0c43e
jermspeaks <jermspeaks@gmail.com>
2014-08-19 17:47:04 -0500

    state machines live

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,36 +1,60 @@
 class Post < ActiveRecord::Base
-  include PostsHelper
+  include ApplicationHelper
   belongs_to :author, class_name: "User"
   belongs_to :memorial
   has_many :comments, as: :commentable
 
   validates :text, presence: true
+  enum status: [:active, :reported, :pending, :permanent, :archived]
 
-  state_machine initial: :published do
-    state :published, value: 0
-    state :flagged, value: 1
-    state :deleted, value: 2
+  state_machine initial: :neutral do
+    state :neutral, value: 0
+    state :pass, value: 1
+    state :fail, value: 2
+    state :approved, value: 3
+    state :disapproved, value: 4
 
-    event :flag do
-      transition :published => :flagged
+    event :passable do
+      transition :neutral => :pass
+    end
+    
+    event :impassable do
+      transition :neutral => :fail
     end
 
-    event :delete do
-      transition :published => :deleted
+    event :approval do
+      transition [:pass, :fail] => :approved
     end
 
-    event :approve do
-      transition :flagged => :published
+    event :disapproval do
+      transition [:pass, :fail] => :disapproved
     end
 
-    event :accident do
-      transition :deleted => :published
+    event :reapproval do
+      transition :disapproved => :approved
     end
+
+    after_transition :neutral => :pass, :do => :post_passable
+    after_transition :neutral => :fail, :do => :post_impassable
+    after_transition [:pass, :fail] => :approved, :do => :post_approval
+    after_transition [:pass, :fail] => :disapproved, :do => :post_disapproval
+    after_transition :disapproved => :approved, :do => :post_approval
   end
 
+  def post_passable
+    self.update(status: 1)
+  end
 
+  def post_impassable
+    self.update(status: 2)
+  end
 
+  def post_approval
+    self.update(status: 3)
+  end
 
-
+  def post_disapproval
+    self.update(status: 4)
+  end
 
 end
