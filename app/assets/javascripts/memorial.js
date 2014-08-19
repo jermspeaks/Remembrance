7d69cc9f6e5c2c20d16a99ba95585401746468a0
franscan <frscala@gmail.com>
2014-08-18 21:47:39 -0500

    first part ajax forms

--- app/assets/javascripts/memorial.js
+++ app/assets/javascripts/memorial.js
@@ -1,7 +1,65 @@
-
-
-
 $(function(){
+
+
+  // $('#create-memorial').one('click', function(event){
+  //   event.preventDefault();
+  //   $.ajax({
+  //     type: 'GET',
+  //     url: '/admin/memorials/new',
+  //     dataType: 'JSON',
+  //     success: function(createMemorial) {
+  //       console.log(createMemorial);
+  //       $( '.center-links' ).append(createMemorial);
+  //       $("#create-memorial").attr("disabled", "disabled");
+  //       }
+  //     });
+  // });
+
+
+
+  $('#create-memorial').one('click', function(event){
+    event.preventDefault();
+
+    $.get('/admin/memorials/new', function(data){
+       //create jquery object from the response html
+       var $response=$(data);
+       //query the jq object for the values
+       var form = $response.find('.new-memorial-form');
+      $('.center-links').append(form);
+        console.log(response);
+    });
+  });
+
+
+
+  $('#new-memorial-post').on('click', function(event){
+    event.preventDefault();
+
+    $.get('/memorials/3/posts/new', function(data){
+       var $response=$(data);
+       var form = $response.find('.new-post-form');
+      $('.memorial-posts').append(form);
+        console.log(response);
+    });
+  });
+
+
+// $("#create_choice").on("submit", function(event) {
+//         event.preventDefault();
+//         $.ajax({
+//             url: "/surveys/create",
+//             type: "POST",
+//             data: $(this).serialize(),
+//             dataType: "json",
+//             success: function(result) {
+//                 console.log(result);
+//                 $("ul").append("<li>" + result.choice_body + "</li>");
+//                 $("#create_choice input[type = 'text']").val("")
+
+//             }
+//         });
+
+
   $('.attended-right-links').one('click', function(event){
     event.preventDefault();
     $.ajax({
@@ -27,4 +85,7 @@ $(function(){
       }
     });
   });
+
+
+
 });
