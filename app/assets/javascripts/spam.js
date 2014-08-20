aedd7848972dd58fec60b20d427e7835f8ef77cf
jermspeaks <jermspeaks@gmail.com>
2014-08-20 12:44:39 -0500

    and rejection works

--- app/assets/javascripts/spam.js
+++ app/assets/javascripts/spam.js
@@ -32,7 +32,7 @@ $(function(){
         for (var j = 0; j < reported.length; j++) {
           var greenLightUrl = "/admin" + pathName + "/posts/" +reported[j].id + "/green_light";
           var redLightUrl = "/admin" + pathName + "/posts/" +reported[j].id + "/red_light";
-          var render = "<div class='reported-posts'><p>" + reported[j].text + "</p><p>" + reported[j].poster + "</p><a class='accept-post' href='" + greenLightUrl + "'>accept this post?</a> | <a href='" + redLightUrl + "'>reject this post?</a></div>";
+          var render = "<div class='reported-posts'><p>" + reported[j].text + "</p><p>" + reported[j].poster + "</p><a class='accept-post' href='" + greenLightUrl + "'>accept this post?</a> | <a class='reject-post' href='" + redLightUrl + "'>reject this post?</a></div>";
           queue.push(render);
         }
         for (var k = 0; k < queue.length; k++) {
@@ -40,6 +40,7 @@ $(function(){
         }
         showQueue();
         modAccept();
+        modReject();
       }
     }
   });
@@ -69,6 +70,21 @@ $(function(){
   };
 
   // Moderator rejects post
+  var modReject = function(){
+    $('.reject-post').on('click', function(event){
+      event.preventDefault();
+      var pathUrl = $(this).attr('href');
+      $(this).parent().remove();
+      $.ajax ({
+        type: 'PUT',
+        url: pathUrl,
+        dataType: 'JSON',
+        success: function(message){
+          console.log(message);
+        }
+      });
+    });
+  };
 
 }); 
   
